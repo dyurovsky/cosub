1dc05db7256c05efac8a9970d43e7f83069ba2c0
Dan Yurovsky <dyurovsky@gmail.com>
2015-05-12 15:38:51 -0700

    file renames

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='cosub-lc',
+setup(name='cosub_lc',
       version='0.1',
       description='A tool for managing external HITs on Mechanical Turk',
       author='Long Ouyang',
       author_email='long.ouyang@gmail.com',
       url='http://www.github.com/dyurovsky/cosub',
-      packages=['cosub-lc'],
+      packages=['cosub_lc'],
       install_requires = ['boto >= 2.33.0','pytimeparse >= 1.1.2'],
       entry_points = {
         'console_scripts': [
-          'cosub = cosub-lc.runner:go'
+          'cosub = cosub_lc.runner:go'
         ]
       }
 )
