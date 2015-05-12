b1a145cd43afba1348b0a5b468d884de19641bc7
Dan Yurovsky <dyurovsky@gmail.com>
2015-05-12 15:19:54 -0700

    fixing setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='cosub',
+setup(name='cosub-lc',
       version='0.1',
       description='A tool for managing external HITs on Mechanical Turk',
       author='Long Ouyang',
       author_email='long.ouyang@gmail.com',
       url='http://www.github.com/longouyang/cosub',
-      packages=['cosub'],
+      packages=['cosub-lc'],
       install_requires = ['boto >= 2.33.0','pytimeparse >= 1.1.2'],
       entry_points = {
         'console_scripts': [
